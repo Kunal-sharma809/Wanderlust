4818f4cfa521d2e9526e7835523553802978dc49
Kunal-sharma809 <kunnu.8442@gmail.com>
2026-03-27 00:18:35 +0530

    Adding sessions and flash messages in Project

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const Listing = require('./models/listing.js');
 const path = require('path');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
-const wrapAsync = require('./utils/wrapAsync.js');
 const ExpressError = require('./utils/ExpressError.js');
-const { listingSchema } = require("./schema.js");
-const Review = require('./models/review.js');
-const { reviewSchema } = require("./schema.js");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
@@ -37,13 +34,35 @@ app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 
 
+const sessionOptions = {
+    secret: "mysupersecret", // Using this only for development phase only
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+    },
+};
+
+
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
 
-app.use("/listings", listings);
+app.use(session(sessionOptions));
+app.use(flash());
 
+// Success Message to show while saving a new listing
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    next();
+})
+
+
+app.use("/listings", listings);
 app.use("/listings/:id/reviews", reviews);
 
 // app.get("/testListing", async (req, res) => {
